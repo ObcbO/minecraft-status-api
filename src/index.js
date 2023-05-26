80cdc9c43ee00eb73688e546aff28886a794977d
ObcbO <m17089494917@163.com>
2023-05-26 14:21:44 +0800

    try

--- src/index.js
+++ src/index.js
@@ -10,18 +10,14 @@ const je_options = {
   enableSRV: true // SRV record lookup
 };
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
-  if (req.method === 'OPTIONS') {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
 });
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // 定义路由
 app.get('/api/status', async (req, res) => {
   let { ip, port, edition } = req.query;
