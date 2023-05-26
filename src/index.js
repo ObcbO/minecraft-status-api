a661e6560233823f6b83426e54bb99152a8a7351
ObcbO <m17089494917@163.com>
2023-05-26 13:15:38 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const util = require('minecraft-server-util');
 const express = require('express');
 const app = express();
-const options = {
+const be_options = {
+  enableSRV: true // SRV record lookup
+};
+const je_options = {
   timeout: 1000 * 5, // timeout in milliseconds
   enableSRV: true // SRV record lookup
 };
@@ -10,20 +13,32 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // 定义路由
 app.get('/api/status', async (req, res) => {
-  const { ip, port } = req.query;
+  let { ip, port, edition } = req.query;
+  port = parseInt(port) || (edition == "be") ? 19132 : 25565;
+  edition = (edition == "") ? "je" : edition;
+  let response = {
+    ip: ip,
+    port: port,
+    edition: edition
+  };
   try {
-    let info = util.status(ip, parseInt(port) || 25565, options);
+    response['status'] = 'success';
+    switch (edition) {
+      case "be":
+        response['info'] = await util.statusBedrock(ip, port, be_options);
+        break;
+      case "je":
+      default:
+        response['info'] = await util.status(ip, port, je_options);
+    }
   } catch (error) {
-
+    response['status'] = 'fail';
   }
-  util.status(ip, parseInt(port) || 25565, options)
-      .then((result) => console.log(result))
-      .catch((error) => console.error(error));
-  //res.json();
+  res.json(response);
 });
 // 404 错误处理中间件
 app.use((req, res, next) => {
-  // res.redirect('https://www.minecraft.net');
+  res.redirect('https://www.minecraft.net');
 });
 
 // 启动服务器
