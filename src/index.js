c4d6c20aaba1b1345ee9089b8217644c20716b72
ObcbO <m17089494917@163.com>
2023-05-26 14:09:57 +0800

    跨域修复

--- src/index.js
+++ src/index.js
@@ -12,6 +12,16 @@ const je_options = {
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 // 定义路由
 app.get('/api/status', async (req, res) => {
   let { ip, port, edition } = req.query;
